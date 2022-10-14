30c420c5bcc47918bba113c5bb4c5cb4cecaeece
Johannes Langosch <johannes.langosch@googlemail.com>
2022-10-14 18:13:04 +0200

    13 contact form sending email

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { on } from "events";
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 
 type Props = {};
 
 function ContactMe({}: Props) {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:johannes.langosch@nexgo.de?subject=${formData.subject}&body=Hi, my name is ${formData.name} ${formData.message} ${formData.email}`;
+  };
+
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -30,13 +44,32 @@ function ContactMe({}: Props) {
             <p className="text-2xl">myEmail@aol.com</p>
           </div>
         </div>
-        <form className="flex flex-col space-y-2 w-fit mx-auto">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
           <div className="flex space-x-2 ">
-            <input placeholder="Name" className="contactInput" type="text" />
-            <input placeholder="Email" className="contactInput" type="email" />
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            />
           </div>
-          <input placeholder="Subject" className="contactInput" type="text" />
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
           <textarea
+            {...register("message")}
             placeholder="Message"
             className="contactInput resize-none"
           />
